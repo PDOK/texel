a0daf48c0da785f9948c3e0aea85394d6fb4fa9e
Wouter <wouter.visscher@gmail.com>
2022-06-10 14:30:43 +0200

    removed specific gpkg func from interface

--- interface.go
+++ interface.go
@@ -12,10 +12,8 @@ type features []interface{}
 
 type Source interface {
 	ReadFeatures(table, chan feature)
-	GetTableInfo() []table
 }
 
 type Target interface {
-	CreateTables([]table) error
 	WriteFeatures(features, table)
 }
