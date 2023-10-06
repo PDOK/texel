04e9241833c73a04ddb6738a0486f0f592785c06
Roel Arents <roel.arents@kadaster.nl>
2023-10-06 12:44:16 +0200

    processPolygonFunc use pointer result (could be nil)
    PDOK-15685

--- processing/processing.go
+++ processing/processing.go
@@ -68,13 +68,13 @@ func processMultiPolygon(mp geom.MultiPolygon, f processPolygonFunc) geom.MultiP
 	var resultMultiPolygon geom.MultiPolygon
 	for _, p := range mp {
 		if resultPolygon := f(p); resultPolygon != nil {
-			resultMultiPolygon = append(resultMultiPolygon, resultPolygon)
+			resultMultiPolygon = append(resultMultiPolygon, *resultPolygon)
 		}
 	}
 	return resultMultiPolygon
 }
 
-type processPolygonFunc func(p geom.Polygon) geom.Polygon
+type processPolygonFunc func(p geom.Polygon) *geom.Polygon
 
 func ProcessFeatures(source Source, target Target, f processPolygonFunc) {
 	featuresBefore := make(chan Feature)
