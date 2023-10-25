4d04a6db990a9c5ef86df5bda628dc54458d7111
Michiel Korpel <michiel.korpel@kadaster.nl>
2023-10-25 10:58:49 +0200

    Invert check to make it a guard clause

--- snap/snap.go
+++ snap/snap.go
@@ -71,7 +71,7 @@ func addPointsAndSnap(ix *PointIndex, polygon *geom.Polygon) *geom.Polygon {
 
 // deduplication using an implementation of the Knuth-Morris-Pratt algorithm
 //
-//nolint:cyclop,funlen,nestif
+//nolint:cyclop,funlen
 func kmpDeduplicate(newRing [][2]float64) [][2]float64 {
 	dedupedRing := make([][2]float64, len(newRing))
 	copy(dedupedRing, newRing)
@@ -83,137 +83,138 @@ func kmpDeduplicate(newRing [][2]float64) [][2]float64 {
 	visitedPoints := [][2]float64{}
 	for i := 0; i < len(newRing); {
 		vertex := newRing[i]
-		if len(visitedPoints) > 1 && visitedPoints[len(visitedPoints)-2] == vertex {
-			// first step back taken, check backwards through visited points to build reverse segment
-			reverseSegment := [][2]float64{visitedPoints[len(visitedPoints)-1], visitedPoints[len(visitedPoints)-2]}
-			for j := 3; j <= len(visitedPoints); j++ {
-				nextI := i + (j - 2)
-				if nextI <= len(newRing)-1 && visitedPoints[len(visitedPoints)-j] == newRing[nextI] {
-					reverseSegment = append(reverseSegment, visitedPoints[len(visitedPoints)-j])
-				} else {
-					// end of segment
-					break
-				}
-			}
-			// create segment from reverse segment
-			segment := make([][2]float64, len(reverseSegment))
-			copy(segment, reverseSegment)
-			slices.Reverse(segment)
-			// create search corpus: initialise with section of 3*segment length, then continuously
-			// add 2*segment length until corpus contains a point that is not in segment
-			start := i - len(segment)
-			end := start + (3 * len(segment))
-			k := 0
-			corpus := newRing[start:min(end, len(newRing))]
-			for {
-				stop := false
-				// check if (additional) corpus contains a point that is not in segment
-				for _, vertex := range corpus[k:] {
-					if !slices.Contains(segment, vertex) {
-						stop = true
-						break
-					}
-				}
-				// corpus already runs until the end of newRing
-				if end > len(newRing) {
-					stop = true
-				}
-				if stop {
-					break
-				}
-				// expand corpus
-				k = len(corpus)
-				corpus = append(corpus, newRing[end:min(end+(2*len(segment)), len(newRing))]...)
-				end += 2 * len(segment)
-			}
-			// search corpus for all matches of segment and reverseSegment
-			matches := kmpSearchAll(corpus, segment)
-			reverseMatches := kmpSearchAll(corpus, reverseSegment)
-			switch {
-			case len(matches) > 1 && (len(matches)-len(reverseMatches)) == 1:
-				// zigzag found (segment occurs one time more often than its reverse)
-				// mark all but one occurrance of segment for removal
-				sequenceStart := start + len(segment)
-				sequenceEnd := start + matches[len(matches)-1] + len(segment)
-				segmentRec := sortedmap.Record{
-					Key: fmt.Sprintf("%v", segment),
-					Val: [2]int{sequenceStart, sequenceEnd},
-				}
-				indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
-				// skip past matched section and reset visitedPoints
-				i = sequenceEnd
-				visitedPoints = [][2]float64{}
-			case len(matches) > 1 && len(matches) == len(reverseMatches):
-				// multiple backtrace found (segment occurs more than once, and equally as many times as its reverse)
-				// mark all but one occurrance of segment and one occurrance of its reverse for removal
-				sequenceStart := start + (2 * len(segment)) - 1
-				sequenceEnd := start + matches[len(matches)-1] + len(segment)
-				segmentRec := sortedmap.Record{
-					Key: fmt.Sprintf("%v", segment),
-					Val: [2]int{sequenceStart, sequenceEnd},
-				}
-				indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
-				// skip past matched section and reset visitedPoints
-				i = sequenceEnd
-				visitedPoints = [][2]float64{}
-			case len(matches) == 1 && len(reverseMatches) == 1:
-				// backtrace found (segment and its reverse occur exactly once)
-				// no removal necessary, skip past matched section and reset visitedPoints
-				i = start + (2 * len(segment)) - 1
-				visitedPoints = [][2]float64{}
-			default:
-				sequenceStart := start
-				var sequenceEnd int
-				var endPointIdx int
-				if len(reverseMatches) > len(matches) {
-					// segment occurs fewer times than its reverse -- could be an odd zigzag, or a backtrace followed by a triangle or a square
-					// remove the initial backtrace, retain the remaining points (backtrace, triangle, or square)
-					sequenceEnd = start + 2*(len(segment)-1)*len(matches)
-					endPointIdx = start + reverseMatches[len(reverseMatches)-1] + len(segment)
-				} else if len(matches) > 1 && (len(matches)-len(reverseMatches)) > 1 {
-					// segment occurs more than one time more often then its reverse -- same as previous case, but with an initial zigzag instead of an initial backtrace
-					sequenceEnd = start + 2*(len(segment)-1)*len(reverseMatches)
-					endPointIdx = start + matches[len(matches)-1] + len(segment)
-				}
-				segmentRec := sortedmap.Record{
-					Key: fmt.Sprintf("%v", segment),
-					Val: [2]int{sequenceStart, sequenceEnd},
-				}
-				indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
-				// check if remaining points are on a straight line, retain only start and end points if so
-				startPointX := newRing[sequenceEnd][0]
-				startPointY := newRing[sequenceEnd][1]
-				onLine := true
-				furthestDistance := 0.0
-				furthestPointIdx := sequenceEnd
-				for n := sequenceEnd + 1; n < endPointIdx; n++ {
-					if newRing[n][0] != startPointX && newRing[n][1] != startPointY {
-						onLine = false
-						break
-					}
-					pointDistance := math.Sqrt(math.Abs(newRing[n][0]-startPointX) + math.Abs(newRing[n][1]-startPointY))
-					if pointDistance > furthestDistance {
-						furthestDistance = pointDistance
-						furthestPointIdx = n
-					}
-				}
-				if onLine {
-					// remove any intermediate points on the return from the end point to the start point
-					sequenceStart := furthestPointIdx + 1
-					sequenceEnd := endPointIdx - 1
-					segmentRec := sortedmap.Record{
-						Key: fmt.Sprintf("%v", newRing[furthestPointIdx:furthestPointIdx+1]),
-						Val: [2]int{sequenceStart, sequenceEnd},
-					}
-					indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
-				}
-				// skip past matched section and reset visitedPoints
-				i = endPointIdx - 1
-				visitedPoints = [][2]float64{}
-			}
-		} else {
+		// not a step back, continue
+		if len(visitedPoints) <= 1 || visitedPoints[len(visitedPoints)-2] != vertex {
 			visitedPoints = append(visitedPoints, vertex)
 			i++
+			continue
+		}
+		// first step back taken, check backwards through visited points to build reverse segment
+		reverseSegment := [][2]float64{visitedPoints[len(visitedPoints)-1], visitedPoints[len(visitedPoints)-2]}
+		for j := 3; j <= len(visitedPoints); j++ {
+			nextI := i + (j - 2)
+			if nextI <= len(newRing)-1 && visitedPoints[len(visitedPoints)-j] == newRing[nextI] {
+				reverseSegment = append(reverseSegment, visitedPoints[len(visitedPoints)-j])
+			} else {
+				// end of segment
+				break
+			}
+		}
+		// create segment from reverse segment
+		segment := make([][2]float64, len(reverseSegment))
+		copy(segment, reverseSegment)
+		slices.Reverse(segment)
+		// create search corpus: initialise with section of 3*segment length, then continuously
+		// add 2*segment length until corpus contains a point that is not in segment
+		start := i - len(segment)
+		end := start + (3 * len(segment))
+		k := 0
+		corpus := newRing[start:min(end, len(newRing))]
+		for {
+			stop := false
+			// check if (additional) corpus contains a point that is not in segment
+			for _, vertex := range corpus[k:] {
+				if !slices.Contains(segment, vertex) {
+					stop = true
+					break
+				}
+			}
+			// corpus already runs until the end of newRing
+			if end > len(newRing) {
+				stop = true
+			}
+			if stop {
+				break
+			}
+			// expand corpus
+			k = len(corpus)
+			corpus = append(corpus, newRing[end:min(end+(2*len(segment)), len(newRing))]...)
+			end += 2 * len(segment)
+		}
+		// search corpus for all matches of segment and reverseSegment
+		matches := kmpSearchAll(corpus, segment)
+		reverseMatches := kmpSearchAll(corpus, reverseSegment)
+		switch {
+		case len(matches) > 1 && (len(matches)-len(reverseMatches)) == 1:
+			// zigzag found (segment occurs one time more often than its reverse)
+			// mark all but one occurrance of segment for removal
+			sequenceStart := start + len(segment)
+			sequenceEnd := start + matches[len(matches)-1] + len(segment)
+			segmentRec := sortedmap.Record{
+				Key: fmt.Sprintf("%v", segment),
+				Val: [2]int{sequenceStart, sequenceEnd},
+			}
+			indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
+			// skip past matched section and reset visitedPoints
+			i = sequenceEnd
+			visitedPoints = [][2]float64{}
+		case len(matches) > 1 && len(matches) == len(reverseMatches):
+			// multiple backtrace found (segment occurs more than once, and equally as many times as its reverse)
+			// mark all but one occurrance of segment and one occurrance of its reverse for removal
+			sequenceStart := start + (2 * len(segment)) - 1
+			sequenceEnd := start + matches[len(matches)-1] + len(segment)
+			segmentRec := sortedmap.Record{
+				Key: fmt.Sprintf("%v", segment),
+				Val: [2]int{sequenceStart, sequenceEnd},
+			}
+			indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
+			// skip past matched section and reset visitedPoints
+			i = sequenceEnd
+			visitedPoints = [][2]float64{}
+		case len(matches) == 1 && len(reverseMatches) == 1:
+			// backtrace found (segment and its reverse occur exactly once)
+			// no removal necessary, skip past matched section and reset visitedPoints
+			i = start + (2 * len(segment)) - 1
+			visitedPoints = [][2]float64{}
+		default:
+			sequenceStart := start
+			var sequenceEnd int
+			var endPointIdx int
+			if len(reverseMatches) > len(matches) {
+				// segment occurs fewer times than its reverse -- could be an odd zigzag, or a backtrace followed by a triangle or a square
+				// remove the initial backtrace, retain the remaining points (backtrace, triangle, or square)
+				sequenceEnd = start + 2*(len(segment)-1)*len(matches)
+				endPointIdx = start + reverseMatches[len(reverseMatches)-1] + len(segment)
+			} else if len(matches) > 1 && (len(matches)-len(reverseMatches)) > 1 {
+				// segment occurs more than one time more often then its reverse -- same as previous case, but with an initial zigzag instead of an initial backtrace
+				sequenceEnd = start + 2*(len(segment)-1)*len(reverseMatches)
+				endPointIdx = start + matches[len(matches)-1] + len(segment)
+			}
+			segmentRec := sortedmap.Record{
+				Key: fmt.Sprintf("%v", segment),
+				Val: [2]int{sequenceStart, sequenceEnd},
+			}
+			indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
+			// check if remaining points are on a straight line, retain only start and end points if so
+			startPointX := newRing[sequenceEnd][0]
+			startPointY := newRing[sequenceEnd][1]
+			onLine := true
+			furthestDistance := 0.0
+			furthestPointIdx := sequenceEnd
+			for n := sequenceEnd + 1; n < endPointIdx; n++ {
+				if newRing[n][0] != startPointX && newRing[n][1] != startPointY {
+					onLine = false
+					break
+				}
+				pointDistance := math.Sqrt(math.Abs(newRing[n][0]-startPointX) + math.Abs(newRing[n][1]-startPointY))
+				if pointDistance > furthestDistance {
+					furthestDistance = pointDistance
+					furthestPointIdx = n
+				}
+			}
+			if onLine {
+				// remove any intermediate points on the return from the end point to the start point
+				sequenceStart := furthestPointIdx + 1
+				sequenceEnd := endPointIdx - 1
+				segmentRec := sortedmap.Record{
+					Key: fmt.Sprintf("%v", newRing[furthestPointIdx:furthestPointIdx+1]),
+					Val: [2]int{sequenceStart, sequenceEnd},
+				}
+				indicesToRemove.Insert(segmentRec.Key, segmentRec.Val)
+			}
+			// skip past matched section and reset visitedPoints
+			i = endPointIdx - 1
+			visitedPoints = [][2]float64{}
 		}
 	}
 	offset := 0
